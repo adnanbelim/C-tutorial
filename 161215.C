b39ccc8e3e667315cfbaabc62bbbb5e618f96c27
Mohammed Adnan Belim <115971540+adnanbelim@users.noreply.github.com>
2023-06-21 23:55:35 +0530

    C Language Assignments

---
+++ 161215.C
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ #include<conio.h>
+ #include<string.h>
+
+ void main()
+ {
+  clrscr();
+
+  char str[50];
+  int i;
+
+  printf("enter value of string");
+  scanf("%d",str[i]);
+
+  for(i=0;i=!'\0';i++)
+  {
+  printf("your string value is %d",str[i]);
+  }
+  getch();
+  }
